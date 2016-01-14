a8fde9f77321bd96f1526aee01e267e013aebf63
brian_ray <monodesigns@gmail.com>
2016-01-14 10:42:10 -0500

    PSR-2
    Updated to PSR-2 coding standards.

--- src/Randomstring.php
+++ src/Randomstring.php
@@ -4,55 +4,55 @@ namespace monodesigns;
 
 class Randomstring
 {
-	public static $upper  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	public static $lower  = 'abcdefghijklmnopqrstuvwxyz';
-	public static $number = '1234567890';
-	public static $symbol = '!#%&()*+,-./~:;<=>?@^_`{|}';
-	public static $hex    = '1234567890abcdef';
+    public static $upper  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    public static $lower  = 'abcdefghijklmnopqrstuvwxyz';
+    public static $number = '1234567890';
+    public static $symbol = '!#%&()*+,-./~:;<=>?@^_`{|}';
+    public static $hex    = '1234567890abcdef';
 
-	/**
-	 * Generates random strings.
-	 * @param  array $params
-	 * @return  string
-	 */
-	public static function generate(array $params = array())
-	{
-		$defaults = [
-			'length' => 6,
-			'types' => 'upper lower number', // all, upper, lower, number, symbol - space separated
-			'hex' => false,
-			'custom' => ''
-		];
-		
-		$params = array_merge($defaults, $params);
+    /**
+    * Generates random strings.
+     * @param  array $params
+     * @return  string
+     */
+    public static function generate(array $params = array())
+    {
+        $defaults = [
+            'length' => 6,
+            'types' => 'upper lower number', // all, upper, lower, number, symbol - space separated
+            'hex' => false,
+            'custom' => ''
+        ];
 
-		if($params['types'] == 'all') {
-			$params['types'] = 'upper lower number symbol';
-		}
+        $params = array_merge($defaults, $params);
 
-		$types = explode(' ', $params['types']);
-		$charstring = '';
+        if ($params['types'] == 'all') {
+            $params['types'] = 'upper lower number symbol';
+        }
 
-		foreach($types as $type) {
-			$charstring .= self::$$type;
-		}
+        $types = explode(' ', $params['types']);
+        $charstring = '';
 
-		if($params['custom']) {
-			$charstring .= $params['custom'];
-		}
+        foreach ($types as $type) {
+            $charstring .= self::$$type;
+        }
 
-		if($params['hex']) {
-			$charstring = self::$hex;
-		}
+        if ($params['custom']) {
+            $charstring .= $params['custom'];
+        }
 
-		$chararray = str_split($charstring);
+        if ($params['hex']) {
+            $charstring = self::$hex;
+        }
 
-		$randomstring = '';
+        $chararray = str_split($charstring);
 
-		for ($i = 0; $i <= $params['length'] - 1; $i++) {
+        $randomstring = '';
+
+        for ($i = 0; $i <= $params['length'] - 1; $i++) {
             $randomstring .= $chararray[mt_rand(0, strlen($charstring) - 1)];
         }
 
         return $randomstring;
-	}
+    }
 }
